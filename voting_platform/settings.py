daadf059fc50bfd904fe9a2c806bb9c1dea33083
Idris David <idris.david@lmu.edu.ng>
2025-02-02 23:59:05 +0100

    Switch from mail gun to gmail for smtp

--- voting_platform/settings.py
+++ voting_platform/settings.py
@@ -174,6 +174,11 @@ EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+# Alternative Django SMTP settings (e.g., for Gmail)
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
